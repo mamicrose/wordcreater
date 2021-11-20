ceba4ec4e3fd7c8186187d8da4b68989d32ac27d
Muhammet Öztep <trofe.1976@gmail.com>
2021-11-20 18:00:29 +0300

    Update word.php

--- word.php
+++ word.php
@@ -1,6 +1,17 @@
-<?php function rasgele($uzunluk)
-{ $karakterler = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPGRSTUVWXYZ!'^#+$&{/([)]=}?\*-|<>@~"; for($i=0;$i<$uzunluk;$i++)
-{ $keys .= $karakterler{rand(0,86)}; } return $keys; }
-$data =rasgele(8); echo $data."<br>"; header("Refresh:0");
-$dosya= fwrite(fopen("wordlist.txt","a+"),"".$data."\n"); fclose($dosya);
+<?php
+function rasgele($uzunluk){
+  $karakterler = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPGRSTUVWXYZ!'^#+$&{/([)]=}?\*-|<>@~";
+  
+  for($i=0;$i<$uzunluk;$i++){
+    $keys .= $karakterler{rand(0,86)};
+  }
+  return $keys;
+}
+
+$data = rasgele(8);
+echo $data."<br>";
+header("Refresh:0");
+$dosya = fwrite(fopen("wordlist.txt","a+"),"".$data."\n");
+fclose($dosya);
+
 ?>
